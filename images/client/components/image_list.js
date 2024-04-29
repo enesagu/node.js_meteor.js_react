c22d452180c7f38f576a29585f09c5a5f5abc168
Enes Agu <97469455+enesagu@users.noreply.github.com>
2024-04-29 12:21:51 +0300

    create image_list.js

---
+++ images/client/components/image_list.js
@@ -0,0 +1,21 @@
+// create our image list component
+// Import React
+
+import React from "react";
+
+
+// Create our component
+
+
+const ImageList = () =>{
+    return (
+        <ul>
+            <li>Image List</li>
+        </ul>
+    );
+}
+
+// Export our component
+
+
+export default ImageList;
