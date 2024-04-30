5618b2cce3edf6ccdb2446366f962b287dcbbf11
Enes Agu <97469455+enesagu@users.noreply.github.com>
2024-04-30 12:48:03 +0300

    update main.js
    Fix reference error and add insertTask function
    
    - Fix reference error by defining insertTask function in server/main.js
    - Add insertTask function in App.jsx to handle task insertion

--- simple-todos-react/server/main.js
+++ simple-todos-react/server/main.js
@@ -1,8 +1,10 @@
-import {Meteor} from 'meteor/meteor';
-import { TasksCollection } from '../imports/api/TasksCollection';
+import { Meteor } from 'meteor/meteor';
+import { TasksCollection } from '/imports/api/TasksCollection';
+
+const insertTask = taskText => TasksCollection.insert({ text: taskText });
 
 Meteor.startup(() => {
-  if (TasksCollection.find().count() == 0){
+  if (TasksCollection.find().count() === 0) {
     [
       'First Task',
       'Second Task',
