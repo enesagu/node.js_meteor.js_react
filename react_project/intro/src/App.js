dbd5a2a5b9b926ed5e60bc429ac243dd0b7f11e3
Enes Agu <97469455+enesagu@users.noreply.github.com>
2024-05-02 17:24:52 +0300

    work with state and components

--- react_project/intro/src/App.js
+++ react_project/intro/src/App.js
@@ -2,19 +2,31 @@ import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
+
+
+
+
+
 
 function App() {
+  let productInfo = {title: "ProductList",someThingElse:"something"};
+  let categoryInfo = {title: "CategoryList"};
   return (
     <div>
       <Container>
-        <Row></Row>
-        <Row></Row>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col cs="3">
+            <CategoryList info={categoryInfo} />
+          </Col>
+          <Col xs="9">
+            <ProductList info={productInfo} />
+          </Col>
+        </Row>
       </Container>
-      <h3>Hello from react</h3>
-      <Navi />
-      <CategoryList />
-      <ProductList />
     </div>
   );
 }
--- react_project/intro/src/CategoryList.js
+++ react_project/intro/src/CategoryList.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Ayakkabı" },
+        { categoryId: 2, categoryName: "Terlik" },
+      ],
+    }; // state burada doğru şekilde tanımlanıyor
+  }
+
   render() {
     return (
       <div>
-        <h3>Catefory List</h3>
+        <h3>{this.props.info.title}</h3>
+
+        <ListGroup>
+          {
+
+            this.state.categories.map(category=>(
+                <ListGroupItem key={category.categoryId}>
+                    {category.categoryName}
+                </ListGroupItem>
+            ))
+           
+          }
+        </ListGroup>
       </div>
-    )
+    );
   }
 }
