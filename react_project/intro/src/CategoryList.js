fc5fcc6d74ee23e1140359c21ec255f56a99daa2
Enes Agu <97469455+enesagu@users.noreply.github.com>
2024-05-02 17:30:57 +0300

    update CategoryList.js
    added onClick event

--- react_project/intro/src/CategoryList.js
+++ react_project/intro/src/CategoryList.js
@@ -2,15 +2,13 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      categories: [
-        { categoryId: 1, categoryName: "Ayakkabı" },
-        { categoryId: 2, categoryName: "Terlik" },
-      ],
-    }; // state burada doğru şekilde tanımlanıyor
-  }
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Ayakkabı" },
+      { categoryId: 2, categoryName: "Terlik" },
+    ],
+    currentCategory : "" 
+  }; // state burada doğru şekilde tanımlanıyor
 
   render() {
     return (
@@ -18,16 +16,13 @@ export default class CategoryList extends Component {
         <h3>{this.props.info.title}</h3>
 
         <ListGroup>
-          {
-
-            this.state.categories.map(category=>(
-                <ListGroupItem key={category.categoryId}>
-                    {category.categoryName}
-                </ListGroupItem>
-            ))
-           
-          }
+          {this.state.categories.map((category) => (
+            <ListGroupItem onClick={() => this.setState({currentCategory: category.categoryName})} key={category.categoryId}>
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
         </ListGroup>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     );
   }
